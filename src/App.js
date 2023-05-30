d95849940fa77ddc9296503effc382ad01d639f8
Fred von Graf <fvongraf@gmail.com>
2023-05-30 01:37:15 -0700

    Update App.js
    Cleaned up code and now using external file for Project ID

--- src/App.js
+++ src/App.js
@@ -1,55 +1,47 @@
-import './App.css';
-import ConnectHomePage from './pages/ConnectHomePage'
-import SendPage from './pages/SendPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Button, Web3Modal } from '@web3modal/react'
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
-//import { useAccount, useContract } from 'wagmi'
-//import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+import { Web3Modal } from '@web3modal/react';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
+
+import ConnectHomePage from './pages/ConnectHomePage';
+import SendPage from './pages/SendPage';
 import './App.css';
 
+// Import PROJECT_ID from constants.js
+import { PROJECT_ID } from './constants';
 
+const CHAINS = [mainnet];
 
-const projectId = "4b2367cdd661553089d9366b1dda20b8"
-const chains = [mainnet]
-
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-
-const tele = window.Telegram.WebApp;
+const { publicClient } = configureChains(CHAINS, [w3mProvider({ projectId: PROJECT_ID })]);
 
 const wagmiConfig = createConfig({
     autoConnect: true,
     connectors: w3mConnectors({
-        projectId: "4b2367cdd661553089d9366b1dda20b8",
+        projectId: PROJECT_ID,
         version: 1,
-        chains
+        chains: CHAINS
     }),
     publicClient
-})
+});
 
-const ethereumClient = new EthereumClient(wagmiConfig, chains)
-
+const ethereumClient = new EthereumClient(wagmiConfig, CHAINS);
 
 function App() {
   return(
-      <div>
+    <div>
       <WagmiConfig config={wagmiConfig}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<ConnectHomePage />} />
-          <Route path='/home' element={<ConnectHomePage />} />
-          <Route path="/send" element={<SendPage />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<ConnectHomePage />} />
+            <Route path='/home' element={<ConnectHomePage />} />
+            <Route path="/send" element={<SendPage />} />
+          </Routes>
+        </BrowserRouter>
       </WagmiConfig>
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-      
+      <Web3Modal projectId={PROJECT_ID} ethereumClient={ethereumClient} />
     </div>
-    
   );
-
 }
 
 export default App;
