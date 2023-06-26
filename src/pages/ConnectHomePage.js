e43b2a6da7676ece6d28b2b83a6f0eb2738133ae
Totis <nottotis@protonmail.com>
2023-06-26 16:28:43 +0900

    Test open metamask android

--- src/pages/ConnectHomePage.js
+++ src/pages/ConnectHomePage.js
@@ -1,12 +1,10 @@
 //import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Button, Web3Modal } from '@web3modal/react'
+import { Web3Button, Web3Modal } from "@web3modal/react";
 //import { configureChains, createConfig, WagmiConfig } from 'wagmi'
 //import { arbitrum, mainnet, polygon } from 'wagmi/chains'
-import { useAccount, useContract } from 'wagmi'
-import { useEffect } from 'react'
-import '../App.css';
-
-
+import { useAccount, useContract } from "wagmi";
+import { useEffect } from "react";
+import "../App.css";
 
 // const projectId = process.env.WALLET_CONNECT_PROJECT_ID
 // const chains = [mainnet, polygon]
@@ -27,36 +25,39 @@ const tele = window.Telegram.WebApp;
 
 // const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
-export default function ConnectHomePage(){
-    const { address, isConnected } = useAccount();
+export default function ConnectHomePage() {
+  const { address, isConnected } = useAccount();
   useEffect(() => {
     tele.ready();
     tele.expand();
   });
 
-  if(!isConnected){
-    return(
-    <div className="App">
-      <header className="App-header">
-      {/* <WagmiConfig config={wagmiConfig}>
+  if (!isConnected) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <WagmiConfig config={wagmiConfig}>
         <Web3Button />
       </WagmiConfig>
         <Web3Modal projectId={projectId} ethereumClient={ethereumClient} /> */}
-        <Web3Button />
-      </header>
-    </div>
-    )
+          <Web3Button />
+          <a href="https://metamask.app.link/">Open metamask</a>
+        </header>
+      </div>
+    );
   }
-  
-  tele.MainButton.setText("Finish").show().onClick(function(){
-    const data = JSON.stringify({address: address})
-    tele.sendData(data);
-    tele.close();
-  });
+
+  tele.MainButton.setText("Finish")
+    .show()
+    .onClick(function () {
+      const data = JSON.stringify({ address: address });
+      tele.sendData(data);
+      tele.close();
+    });
   return (
     <div className="App">
       <header className="App-header">
-      {/* <WagmiConfig config={wagmiConfig}>
+        {/* <WagmiConfig config={wagmiConfig}>
       <div>
               
               <h3>Wallet Connected</h3>
@@ -67,13 +68,11 @@ export default function ConnectHomePage(){
       </WagmiConfig>
         <Web3Modal projectId={projectId} ethereumClient={ethereumClient} /> */}
         <div>
-              
-              <h3>Wallet Connected</h3>
-              <p>Address: {address}</p>
-              <Web3Button />
-      </div>
+          <h3>Wallet Connected</h3>
+          <p>Address: {address}</p>
+          <Web3Button />
+        </div>
       </header>
     </div>
   );
-
 }
